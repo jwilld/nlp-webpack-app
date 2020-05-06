b58136fec3ef1228fd3088107bb3dd1010bbeeb5
Jerry Davidson <willdjerry@gmail.com>
2020-05-06 01:23:17 -0400

    add tests for .js files in js folder

---
+++ src/client/js/formHandler.test.js
@@ -0,0 +1,1 @@
+const handleSubmit = require('./formHandler')
---
+++ src/server/index.js
@@ -0,0 +1,58 @@
+// api key/ id
+const dotenv = require("dotenv");
+dotenv.config();
+const api_key = process.env.API_KEY;
+const app_id = process.env.API_ID;
+
+
+let path = require("path");
+const express = require("express");
+const mockAPIResponse = require("./mockAPI.js");
+
+//aylien api
+const aylienAPI = require("aylien_textapi");
+const urlapi = new aylienAPI({
+  application_key: api_key,
+  application_id: app_id,
+});
+
+const getSentiment = async () => {
+  const sentiment = await urlapi.sentiment(
+    {
+      url:
+        "https://www.cnn.com/2020/05/05/us/naked-bike-ride-portland-coronavirus-trnd/index.html",
+    },
+    (error, response) => {
+      if (error === null) {
+        return response;
+      } else {
+        console.log(error);
+      }
+    }
+  )
+  return sentiment
+};
+
+
+
+const app = express();
+
+app.use(express.static("dist"));
+
+console.log(__dirname);
+
+app.get("/", function (req, res) {
+  // res.sendFile('dist/index.html')
+  res.sendFile(path.resolve("src/client/views/index.html"));
+});
+
+// designates what port the app will listen to for incoming requests
+app.listen(8080, function () {
+  console.log("Example app listening on port 8080!");
+});
+
+app.get("/test", function (req, res) {
+  res.send(mockAPIResponse);
+});
+
+module.exports = getSentiment;
