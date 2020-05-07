ca098a6599485dd7b572ab6cf51bedb35e34a5f9
Jerry Davidson <willdjerry@gmail.com>
2020-05-07 13:41:45 -0400

    add layout from Figma

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -3,7 +3,7 @@ function handleSubmit(event) {
   event.preventDefault();
 
 //   check what text was put into the form field
-  let formText = document.getElementById("name").value;
+  let formText = document.getElementById("url").value;
   checkForName(formText);
 
   console.log("::: Form Submitted :::");
